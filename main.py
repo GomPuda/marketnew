102ebc012fbb71265d2bd54a5b2535866c6632df
GomPuda <swoni12@naver.com>
2023-10-08 21:08:44 +0900

    마켓클론 코딩 프로토 타입
    아직 멀었습니다~

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI, UploadFile,Form,Response
+from fastapi import FastAPI, UploadFile,Form,Response,Depends
 # 필기
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from fastapi.staticfiles import StaticFiles
+from fastapi_login import LoginManager
+from fastapi_login.exceptions import InvalidCredentialsException
 from typing import Annotated
 import sqlite3
 
@@ -11,13 +13,63 @@ cur = con.cursor()
 
 app = FastAPI()
 
+SECRET = "coding"
+manager = LoginManager(SECRET,'/login')
+
+@manager.user_loader
+def query_user(data):
+    WHERE_STATEMENTS = f'id="{data}"'
+    if type (data) == dict:
+        WHERE_STATEMENTS = f'''id="{data["id"]}"'''
+        
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    user = cur.execute(f"""
+                       SELECT * from users WHERE {WHERE_STATEMENTS}
+                       """).fetchone()  
+    return user
+
+@app.post('/login')
+def login(id:Annotated[str,Form()],
+           password:Annotated[str,Form()]):
+    user = query_user(id)
+    if not user:
+        raise InvalidCredentialsException
+    elif password != user['password']:
+        raise InvalidCredentialsException
+    
+    access_token = manager.create_access_token(data = {
+        "sub": {
+        "id":user['id'],
+        "name":user['name'],
+        "email":user['email']
+        }
+    })
+     
+    return {"access_token":access_token}
+
+@app.post("/signup")
+def signup(id:Annotated[str,Form()],
+           password:Annotated[str,Form()],
+           name:Annotated[str,Form()],
+           email:Annotated[str,Form()]):
+    cur.execute(f"""
+                INSERT INTO users(id,name,email,password)
+                VALUES ('{id}','{name}','{email}','{password}')
+                """)
+    con.commit()
+    return "200"
+    
+
+
 @app.post("/items")
 async def create_item(image:UploadFile,
                 title:Annotated[str,Form()],
                 price:Annotated[int,Form()],
                 description:Annotated[str,Form()],
                 place:Annotated[str,Form()],
-                insertAT:Annotated[int,Form()]):
+                insertAT:Annotated[int,Form()],
+                user=Depends(manager)):
     print(image,title,price,description,place,insertAT)
     image_bytes = await image.read()
     
@@ -29,7 +81,7 @@ async def create_item(image:UploadFile,
     return "200"
 # 필기
 @app.get("/items")
-async def get_items():
+async def get_items(user=Depends(manager)):
     # 컬럼명도 같이 가져옴
     con.row_factory= sqlite3.Row 
     cur = con.cursor()
@@ -48,16 +100,7 @@ async def get_image(item_id):
     
     return Response(content=bytes.fromhex(image_bytes))
 
-@app.post("/signup")
-def signup(id:Annotated[str,Form()],
-           password:Annotated[str,Form()],
-           name:Annotated[str,Form()],
-           email:Annotated[str,Form()]):
-    cur.execute(f"""
-                INSERT INTO users(id,name,email,password)
-                VALUES ('{id}','{name}','{email}','{password}')
-                """)
-    con.commit()
-    return "200"
+
 
 app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
+
